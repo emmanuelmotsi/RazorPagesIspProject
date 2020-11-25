ffe36991db2b0d0eb26775459bf805b0056313b2
Emmanuel Motsi <emmanuelmotsi@gmail.com>
2020-11-25 18:11:37 +0200

    Interface edits.

--- RazorPagesIspProject/Pages/Companies/Applications.cshtml.cs
+++ RazorPagesIspProject/Pages/Companies/Applications.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RazorPagesIspProject.Pages
+namespace RazorPagesIspProject.Pages.Companies
 {
-    public class ContactModel : PageModel
+    public class ApplicationsModel : PageModel
     {
         public void OnGet()
         {
